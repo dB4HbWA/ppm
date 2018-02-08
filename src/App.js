9ad3de0fee6b5aef225802f85bf1e1078ddc3b6d
DOBROWITS, JOE <joedobrowits@northwesternmutual.com>
2018-02-08 11:48:08 -0600

    HomeView Complete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './ui-toolkit/css/nm-cx/main.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  withRouter // a higher order component
+} from 'react-router-dom'
+import HomeView from './HomeView'
+// import MineView from './MineView'
+// import BuyView from './BuyView'
+// import SellView from './SellView'
+// import LedgerView from './LedgerView'
+// import TransactionView from './TransactionView'
+
+const NmTab = (props) => {
+  return (
+    
+    <Route exact={props.exact} path={props.to} children={({match}) => {
+      return (
+        <li className={`tab-title ${match ? 'active' : ''}`} >
+          <Link to={props.to}>{props.tabName}</Link>
+        </li>
+      )}
+    } />
+  )
+}
+
+const ButtonGroup = props => (
+  <nav className='NavBar'>
+  <h3 align='left'>PPM - Project Product Management</h3>
+    <ul className="tabs">
+      {/* calling NmTab to generate the Route, circumvents NM styling and Anchor tags */}
+      <NmTab to={"/home"} tabName="Home" />
+      <NmTab exact={true} to={"/products"} tabName="Product List" />
+      <NmTab to={"/products/new"} tabName="Product Creation" />
+    </ul>
+  </nav>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <ButtonGroup />
+          <Switch>
+            <Route path="/home" component={HomeView} />
+            {/* <Route path="/products" component={ProductsView} />
+            <Route path="/products/new" component={NewProductView} />
+            <Route path="/products/edit/:id" component={EditView} /> */}
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
+
---
+++ src/HomeView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const HomeView = props => (
+  <div className='homeView'>
+    <h1>Home Page</h1>
+    <p>Welcome to the Project Product Management.  Here we can manage a set of products.  You are able to create new products, remove old produts, and edit products.</p>
+  </div>
+)
+
+export default HomeView
